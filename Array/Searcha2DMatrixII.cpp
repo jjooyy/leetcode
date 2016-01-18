b13253f3c6e7c99302fe00a0e727c856f6cb5b90
Qiaoyi Xu <youranjoy@gmail.com>
2016-01-18 17:36:37 -0500

    Create Searcha2DMatrixII.cpp

---
+++ Array/Searcha2DMatrixII.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int row = matrix.size();
+        if(!row) {
+            return false;
+        }
+        int col = matrix[0].size();
+        if(!col) {
+            return false;
+        }
+        int i = 0;
+        int j = col - 1;
+        while(i < row && j >= 0) {
+            if(matrix[i][j] == target) {
+                return true;
+            }
+            else if(matrix[i][j] < target) {
+                i++;
+            }
+            else {
+                j--;
+            }
+        }
+        return false;
+    }
+};
